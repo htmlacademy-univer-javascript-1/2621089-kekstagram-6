70d6fa2abd9fa10b8eb9adba8fee5b02ee7b07e0
Милана <milana42297@gmail.com>
2025-10-06 15:40:21 +0500

    add new function

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+function checkStringLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+function isPalindrome(str) {
+    return cleanStr === cleanStr.split('').reverse().join('');
+}
